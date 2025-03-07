a1447dc79050d7b34a01f35d0e59d831552e6172
Ninis <MiguelAdolfoV@outlook.com>
2025-03-06 21:14:37 -0600

    Agregado diseño de dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
 
 ];
 
