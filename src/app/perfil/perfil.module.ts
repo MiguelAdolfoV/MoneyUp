6ae2bb4c688aa53f77e45a021dff2210a6e04765
michellebvmm <michelle.1807bm@gmail.com>
2025-03-26 12:15:16 -0600

    module

---
+++ src/app/perfil/perfil.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { PerfilPageRoutingModule } from './perfil-routing.module';
+import { PerfilPage } from './perfil.page';
+
+
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PerfilPageRoutingModule,
+    IonicStorageModule.forRoot() 
+  ],
+  declarations: [PerfilPage]
+})
+export class PerfilPageModule {}
